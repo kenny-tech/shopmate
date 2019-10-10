1f80cfba010f794c1a66e89a9f3b208ad34f0fe0
kenny-tech <kenny4real2001@gmail.com>
2019-10-10 12:10:29 +0100

    Installed redux and redux thunk

---
+++ src/components/Product.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-10">
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Product;
