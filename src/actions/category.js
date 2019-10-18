fce029a205f1d9beafe9f62aefe2b0d5679ea8df
kenny-tech <kenny4real2001@gmail.com>
2019-10-18 15:35:58 +0100

    Updated modal css

--- src/actions/category.js
+++ src/actions/category.js
@@ -9,7 +9,6 @@ export function fetchDepartmentCategories(department_id)
     return (dispatch) => {
         axios.get(ROOT_URL + 'categories/inDepartment/'+ department_id)
         .then(response => {
-            // console.log('Department ID: ', response);
             dispatch({
                 type: FETCH_DEPARTMENT_CATEGORIES,
                 payload: response.data
