683d4e76166faa0b71c4baf9aff0474889a433a8
kenny-tech <kenny4real2001@gmail.com>
2019-10-24 22:48:09 +0100

    Pagination in progress

--- src/actions/product.js
+++ src/actions/product.js
@@ -4,9 +4,9 @@ import { FETCH_PRODUCT } from './types';
 
 const ROOT_URL = 'https://backendapi.turing.com/';
 
-
 export const fetchProducts = () => {
     return (dispatch) => {
+        //axios.get(ROOT_URL + 'products?page='+page+'&limit='+limit)
         axios.get(ROOT_URL + 'products')
         .then(response => {
             //console.log(response);
@@ -35,7 +35,7 @@ export const fetchSearchProduct = (searchText) => {
     return (dispatch) => {
         axios.get(ROOT_URL + 'products/search/?query_string=' + searchText)
         .then(response => {
-            console.log('Search Product: ', response);
+            //console.log('Search Product: ', response);
             dispatch({
                 type: FETCH_PRODUCT,
                 payload: response.data
--- src/components/Product.js
+++ src/components/Product.js
@@ -9,12 +9,25 @@ import '../App.css';
 const imageBaseUrl = 'https://backendapi.turing.com/images/products/';
 
 class Product extends Component {
+    state = {
+        currenPage: 1,
+        perPage: 6
+    }
+
     componentWillMount() {
         this.props.fetchProducts();   
     }
 
+    handleClick = (number) => {
+        this.setState({
+            currentPage: Number(number)
+        });  
+    }
+
     render() {
         
+        const { currentPage, perPage } = this.state;
+
         if(!this.props.products) {
             return <div>Loading...</div>
         }
@@ -22,26 +35,57 @@ class Product extends Component {
         const productArray = Object.values(this.props.products.rows);
         //console.log('The product array: ', productArray);
 
+        // Logic for displaying current products
+        const indexOfLastProduct = currentPage * perPage;
+        const indexOfFirstProduct = indexOfLastProduct - perPage;
+        const currentProducts = productArray.slice(indexOfFirstProduct, indexOfLastProduct);
+
+        const renderProducts = currentProducts.map((product, index) => {
+            return (
+                <div className="col-md-3 productDiv">
+                    <div className="card">
+                        <img src={imageBaseUrl + product.thumbnail} className="card-img-top img-responsive" alt={product.name} />
+                        <div className="card-body">
+                            <p className="card-title text-center productTitle">{product.name}</p>
+                            <p className="card-text text-center textPink">${product.price}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        });
+
+        // Logic for displaying page numbers
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(productArray.length / perPage); i++) {
+          pageNumbers.push(i);
+        }
+
+        const renderPageNumbers = pageNumbers.map(number => {
+            return (
+              <li
+                key={number}
+                id={number}
+                onClick={() => this.handleClick(number)}
+              >
+                {number}
+              </li>
+            );
+        });
+
+
         return (
             <React.Fragment>    
                 <div className="container-fluid p-3">
                     <div className="row">
+                        <div className="col-md-12 text-center">
+                            <ul className="d-inline">
+                                {renderPageNumbers}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="row">
                         <div className="col-md-9 row">
-                            {
-                                productArray.map(product => {
-                                    return (
-                                            <div className="col-md-3 productDiv">
-                                                <div className="card">
-                                                    <img src={imageBaseUrl + product.thumbnail} className="card-img-top img-responsive" alt={product.name} />
-                                                    <div className="card-body">
-                                                        <p className="card-title text-center productTitle">{product.name}</p>
-                                                        <p className="card-text text-center textPink">${product.price}</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                    )
-                                })
-                            }
+                            { renderProducts }
                         </div>
                     </div>
                 </div>
