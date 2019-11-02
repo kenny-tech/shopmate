295dd379823bd11caae8df0960e695effa529f8d
kenny-tech <kenny4real2001@gmail.com>
2019-11-02 22:18:57 +0100

    Updating product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,43 +1,82 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { connect } from 'react-redux';
 import { fetchProducts } from '../actions/product';
 import { addToCart } from '../actions/cart';
-import Button from '../components/Button';
 
 import '../css/Product.css';
 import '../App.css';
 
 const imageBaseUrl = 'https://backendapi.turing.com/images/products/';
 
-class Product extends Component {
+class Product extends React.PureComponent {
+
+    componentWillMount() {
+        this.props.fetchProducts();   
+    }
 
     state = {
         hover: false,
         productId: 0,
         size: 'm',
-        color: 'white'
-    }
-
-    componentWillMount() {
-        this.props.fetchProducts();   
+        color: 'white',
+        products: this.props.products
     }
 
     toggleHover = (product_id) => {
         this.setState({
             hover: !this.state.hover,
             productId: product_id
-        })
+        });
+        console.log('Current state is: ', this.state);
     }
 
     render() {
 
+        console.log('rendering...');
+        
         if(!this.props.products) {
             return <div>Loading...</div>
         }
                 
-        
-        const productArray = Object.values(this.props.products.rows);
+        const renderProducts = Object.values(this.props.products.rows).map((product, index) => {
+            if(this.state.hover || this.state.productId === product.product_id) {
+                return (<div className="col-md-3 productDiv" onMouseEnter={() => this.toggleHover(product.product_id)} onMouseLeave={() => this.toggleHover()} style={{ cursor: 'pointer' }}>
+                    <div className="card productCard">
+                        <div className="card-body">
+                            <p className="card-title text-center productTitle">{product.name}</p>
+                            <p className="card-text text-center textPink">${product.price}</p>
+                            <form className="form-inline">
+                                <select className="form-control form-control-sm" id="inlineFormCustomSelectPref">
+                                    <option value="s">S</option>
+                                    <option value="m">M</option>
+                                    <option value="l">L</option>
+                                </select>
+                                <select className="form-control form-control-sm" id="inlineFormCustomSelectPref">
+                                    <option value="white">White</option>
+                                    <option value="black">Black</option>
+                                    <option value="yellow">Yellow</option>
+                                </select>
+                            </form>
+                            <p className="text-center"><button>Add to cart</button></p>
+                        </div>
+                    </div>
+                </div>)
+            } 
+            return (
+                <div className="col-md-3 productDiv" onMouseEnter={() => this.toggleHover()} onMouseLeave={() => this.toggleHover()} style={{ cursor: 'pointer' }}>
+                    <div className="card productCard">
+                        <img src={imageBaseUrl + product.thumbnail} className="card-img-top img-responsive" alt={product.name} />
+                        <div className="card-body">
+                            <p className="card-title text-center productTitle">{product.name}</p>
+                            <p className="card-text text-center textPink">${product.price}</p>
+                        </div>
+                    </div>
+                </div>
+            ) 
+        });
+
+        // const productArray = Object.values(this.props.products.rows);
         //console.log('The product array: ', productArray);
 
         return (
@@ -45,47 +84,7 @@ class Product extends Component {
                 <div className="container-fluid p-3">
                     <div className="row">
                         <div className="col-md-9 row">
-                            {
-                                productArray.map(product => {
-                                    { 
-                                        if(this.state.hover || this.state.productId === product.product_id) {
-                                            return (<div className="col-md-3 productDiv" onMouseEnter={() => this.toggleHover(product.product_id)} onMouseLeave={() => this.toggleHover()} style={{ cursor: 'pointer' }}>
-                                                <div className="card productCard">
-                                                    <div className="card-body">
-                                                        <p className="card-title text-center productTitle">{product.name}</p>
-                                                        <p className="card-text text-center textPink m-b-25">${product.price}</p>
-                                                        <form className="form-inline d-flex justify-content-center formContent">
-                                                            <select className="form-control form-control-sm selectSpacing">
-                                                                <option value="s">S</option>
-                                                                <option value="m">M</option>
-                                                                <option value="l">L</option>
-                                                            </select>
-                                                            <select className="form-control form-control-sm">
-                                                                <option value="white">White</option>
-                                                                <option value="black">Black</option>
-                                                                <option value="yellow">Yellow</option>
-                                                            </select>
-                                                        </form>
-                                                        <p className="text-center"><button className="btn btn-default btn-xs buttonAddToCart" onClick={()=>this.props.addToCart(product.product_id,product.name,product.price,this.state.size,this.state.color)}>Add to cart</button></p>
-                                                    </div>
-                                                </div>
-                                            </div>)
-
-                                        } 
-                                        return (
-                                            <div className="col-md-3 productDiv" onMouseEnter={() => this.toggleHover()} onMouseLeave={() => this.toggleHover()} style={{ cursor: 'pointer' }}>
-                                                <div className="card productCard">
-                                                    <img src={imageBaseUrl + product.thumbnail} className="card-img-top img-responsive" alt={product.name} />
-                                                    <div className="card-body">
-                                                        <p className="card-title text-center productTitle">{product.name}</p>
-                                                        <p className="card-text text-center textPink">${product.price}</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ) 
-                                    }
-                                })
-                            }
+                            {renderProducts}
                         </div>
                     </div>
                 </div>
