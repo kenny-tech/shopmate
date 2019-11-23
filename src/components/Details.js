ddaa7b82b3cdae2f60ddf25d1413867dee8b97f5
kenny-tech <kennyemma2008@gmail.com>
2019-11-23 22:30:55 +0100

    Product details page in progress

--- src/components/Details.js
+++ src/components/Details.js
@@ -17,7 +17,7 @@ class Details extends Component {
         return (
             <div className="container m-3 d-flex">
                 <div>
-                    <div class="panel panel-default divPanel">
+                    <div class="panel panel-default divPanel w-100 p-3">
                         <div class="panel-body">
                             {this.props.detail.map(product => {
                                 return (
@@ -27,8 +27,28 @@ class Details extends Component {
                                         </div>
                                         <div className="col-md-9">
                                             <p>{product.name}</p>
-                                            <h3>${product.price}</h3>
+                                            <h3 className="priceText">${product.price}</h3>
                                             <p>{product.description}</p>
+                                            <h6>Quantity</h6>
+                                            <input type="number" className="form-control" style={{width:'80px'}} defaultValue={product.quantity} min="1"/>
+                                            <h6>Size</h6>
+                                            <div className="row">
+                                                <div className="col-md-2">
+                                                    <div className="divSize d-flex justify-content-center align-items-center d-inline-block">S</div>
+                                                </div>
+                                                <div className="col-md-2">
+                                                    <div className="divSize d-flex justify-content-center align-items-center d-inline-block">M</div>
+                                                </div>
+                                                <div className="col-md-2">
+                                                    <div className="divSize d-flex justify-content-center align-items-center d-inline-block">L</div>
+                                                </div>
+                                                <div className="col-md-2">
+                                                    <div className="divSize d-flex justify-content-center align-items-center d-inline-block">XL</div>
+                                                </div>
+                                                <div className="col-md-2">
+                                                    <div className="divSize d-flex justify-content-center align-items-center d-inline-block">XXL</div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 )
