b3d7f10c16b5104b19698066ad671462cc9425e7
kenny-tech <kennyemma2008@gmail.com>
2019-11-24 17:05:55 +0100

    Implemented product details page functionality

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Button from '../components/Button';
-
+import { addToCart } from '../actions/cart';
 import '../css/Details.css';
 
 const imageBaseUrl = 'https://backendapi.turing.com/images/products/';
 
 class Details extends Component {
+    state = {
+        quantity: 1,
+        size: 'S',
+        color: 'White',
+        sizeBgColor: '',
+        sizeTextColor: '',
+        colorBgColor: '',
+        colorTextColor: ''
+    }
+
+    handleQuantityChange = (event) => {
+        this.setState({quantity: event.target.value});
+    }
+
+    handleSizeChange = (size) => {
+        this.setState({size: size, sizeBgColor: '#FF1493', sizeTextColor: '#FFFFFF'});
+    }
+    
+    handleColorChange = (color) => {
+        this.setState({color: color, colorBgColor: '#FF1493', colorTextColor: '#FFFFFF'});
+    }
+
+    handleAddToCartClick = (product_id,product,price,thumbnail,quantity,size,color) => {
+        this.props.addToCart(product_id,product,price,thumbnail,quantity,size,color);
+        alert('Product successfully added to cart');
+    }
     
     render() {
         if(!this.props.detail) {
@@ -22,7 +47,7 @@ class Details extends Component {
                             {this.props.detail.map(product => {
                                 return (
                                     <form>
-                                        <div className="row p-3">
+                                        <div className="row p-3" key={product.product_id}>
                                             <div className="col-md-3">
                                                 <img src={imageBaseUrl + product.thumbnail} alt={product.name} />
                                             </div>
@@ -31,56 +56,56 @@ class Details extends Component {
                                                 <h3 className="priceText">${product.price}</h3>
                                                 <p>{product.description}</p>
                                                 <h6>Quantity</h6>
-                                                <input type="number" className="form-control" style={{width:'80px'}} defaultValue={product.quantity} min="1"/>
+                                                <input type="number" className="form-control" style={{width:'80px'}} defaultValue={product.quantity} onChange={this.handleQuantityChange} min="1"/>
                                                 <h6 className="mt-3">Size</h6>
                                                 <div className="row mt-3">
                                                     <div className="col-md-2">
-                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block">S</div>
+                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleSizeChange('S')} style={{ cursor: 'pointer', backgroundColor: this.state.sizeBgColor, color: this.state.sizeTextColor }}>S</div>
                                                     </div>
                                                     <div className="col-md-2">
-                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block">M</div>
+                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleSizeChange('M')} style={{ cursor: 'pointer' }}>M</div>
                                                     </div>
                                                     <div className="col-md-2">
-                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block">L</div>
+                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleSizeChange('L')} style={{ cursor: 'pointer' }}>L</div>
                                                     </div>
                                                     <div className="col-md-2">
-                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block">XL</div>
+                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleSizeChange('XL')} style={{ cursor: 'pointer' }}>XL</div>
                                                     </div>
                                                     <div className="col-md-2">
-                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block">XXL</div>
+                                                        <div className="divSizeBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleSizeChange('XXL')} style={{ cursor: 'pointer' }}>XXL</div>
                                                     </div>
                                                 </div>
                                                 <h6 className="mt-3">Color</h6>
                                                 <div className="row mt-3">
                                                     <div className="col-md-1">
-                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block">WHITE</div>
+                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleColorChange('White')} style={{ cursor: 'pointer', backgroundColor: this.state.colorBgColor, color: this.state.colorTextColor }}>WHITE</div>
                                                     </div>
                                                     <div className="col-md-1">
-                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block">BLACK</div>
+                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleColorChange('Black')} style={{ cursor: 'pointer' }}>BLACK</div>
                                                     </div>
                                                     <div className="col-md-1">
-                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block">RED</div>
+                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleColorChange('Red')} style={{ cursor: 'pointer' }}>RED</div>
                                                     </div>
                                                     <div className="col-md-1">
-                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block">ORANGE</div>
+                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleColorChange('Orange')} style={{ cursor: 'pointer' }}>ORANGE</div>
                                                     </div>
                                                     <div className="col-md-1">
-                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block">YELLOW</div>
+                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleColorChange('Yellow')} style={{ cursor: 'pointer' }}>YELLOW</div>
                                                     </div>
                                                     <div className="col-md-1">
-                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block">BLUE</div>
+                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleColorChange('Blue')} style={{ cursor: 'pointer' }}>BLUE</div>
                                                     </div>
                                                     <div className="col-md-1">
-                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block">GREEN</div>
+                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleColorChange('Green')} style={{ cursor: 'pointer' }}>GREEN</div>
                                                     </div>
                                                     <div className="col-md-1">
-                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block">INDIGO</div>
+                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleColorChange('Indigo')} style={{ cursor: 'pointer' }}>INDIGO</div>
                                                     </div>
                                                     <div className="col-md-1">
-                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block">PURPLE</div>
+                                                        <div className="divColorBox d-flex justify-content-center align-items-center d-inline-block" onClick = {() => this.handleColorChange('Purple')} style={{ cursor: 'pointer' }}>PURPLE</div>
                                                     </div>
                                                 </div>
-                                                <Button buttonText="Add to cart" />
+                                                <button className="btn btn-default btn-m mt-3 buttonAddToCart" onClick={()=>this.handleAddToCartClick(product.product_id,product.name,product.price,product.thumbnail,this.state.quantity,this.state.size,this.state.color)}>Add to cart</button>
                                             </div>
                                         </div>
                                     </form>
@@ -95,7 +120,17 @@ class Details extends Component {
 }
 
 const mapStateToProps = (state) => {
-    return { detail: state.products.detail}
+    return { 
+        detail: state.products.detail,
+        cart: state.cart.cart,
+    }
 }
 
-export default connect(mapStateToProps, null)(Details);
+const mapDispatchToProps = (dispatch) => {
+    return { 
+            addToCart: (product_id,product,price,thumbnail,quantity,size,color) => {dispatch(addToCart(product_id,product,price,thumbnail,quantity,size,color))}
+        }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Details);
