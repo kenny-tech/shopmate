4659c360b9763c6990cffc498e6fd352a1e225fc
kenny-tech <kennyemma2008@gmail.com>
2019-12-05 17:06:21 +0100

    Implemented signin functionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -12,7 +12,28 @@ export const signupUser = ({ name, email, password }) => {
                     type: AUTH_USER, 
                     payload: true
                 });
-                localStorage.setItem('token', response.accessToken);
+                localStorage.setItem('token', response.data.accessToken);
+                console.log('Registration successful. Your access token is: ',response.data.accessToken);
+            })
+            .catch(err => {
+                dispatch({
+                    type: AUTH_ERROR,
+                    payload: err.message
+                })
+            })
+    }
+}
+
+export const signinUser = ({ email, password }) => {
+    return (dispatch) => {
+        axios.post(`${ROOT_URL}/customers/login`, { email, password })
+            .then(response => {
+                dispatch({ 
+                    type: AUTH_USER, 
+                    payload: true
+                });
+                localStorage.setItem('token', response.data.accessToken);
+                console.log('Login successful. Your access token is: ',response.data.accessToken);
             })
             .catch(err => {
                 dispatch({
