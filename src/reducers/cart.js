676cb86b45c74a62010e840aac83311b69ccd512
kenny-tech <kennyemma2008@gmail.com>
2020-01-17 11:54:48 +0100

    Increase product quantity in progress

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -38,7 +38,7 @@ export const reducer = ( state = initState, action ) => {
             let addedItem = state.cart.find(item => item.product_id === action.payload.product_id)
             // console.log('addedItem: ',addedItem);
             addedItem.quantity += 1
-            addedItem.price += addedItem.price
+            addedItem.price += parseFloat(addedItem.price)
             let newTotal = state.total + parseFloat(addedItem.price)
             return {
                 ...state,
